d508a72bd0cb1f5925bf963f05f93bb288d500f1
Andrew Grimberg <tykeal@bardicgrove.org>
2020-04-17 11:52:21 -0700

    only do puppetserver config
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/puppetserver.pp
+++ manifests/puppetserver.pp
@@ -5,8 +5,8 @@ class role::puppetserver {
   include ::profile::puppetserver
 
   # Puppet servers are currently also PuppetDB
-  include ::profile::puppetdb::postgresql
-  include ::profile::puppetdb::server
+  # include ::profile::puppetdb::postgresql
+  # include ::profile::puppetdb::server
 
-  include ::profile::puppet::master
+  # include ::profile::puppet::master
 }
