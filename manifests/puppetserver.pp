0515bb9a99d571e989af5c1df8b83c7827696ef3
Andrew Grimberg <tykeal@bardicgrove.org>
2020-04-17 11:56:30 -0700

    Configure puppetdb postgresql
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/puppetserver.pp
+++ manifests/puppetserver.pp
@@ -5,7 +5,7 @@ class role::puppetserver {
   include ::profile::puppetserver
 
   # Puppet servers are currently also PuppetDB
-  # include ::profile::puppetdb::postgresql
+  include ::profile::puppetdb::postgresql
   # include ::profile::puppetdb::server
 
   # include ::profile::puppet::master
