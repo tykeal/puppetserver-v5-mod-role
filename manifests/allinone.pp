479c3242342df70bbba5920a90b7b7a1646bb4e9
Andrew Grimberg <tykeal@bardicgrove.org>
2020-06-13 14:58:09 -0700

    Add the allinone role
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

---
+++ manifests/allinone.pp
@@ -0,0 +1,13 @@
+# ::role::allinone
+
+# This is used by circle (bardicgrove.org primary host) It's an all in one
+# profile as we only run one system
+#
+# In truth I would prefer to separate services, but the cost involved in that
+# would make the wife very unhappy, so it's a single all in one system :(
+class role::allinone {
+  include ::profile::base
+
+  # We do MTA services
+  include ::profile::postfix
+}
